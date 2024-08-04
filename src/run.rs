7b20aaabb837b3579833815d2fd9759e8478ae61
Tristan Sloughter <tristan@sloughter.dev>
2024-08-04 16:57:20 -0600

    maybe support windows command runs

--- src/run.rs
+++ src/run.rs
@@ -1,16 +1,48 @@
+use color_eyre::{eyre::Report, eyre::Result};
 use std::env::Args;
-use std::os::unix::prelude::CommandExt;
+
 use std::path::*;
 use std::process::Command;
 
 use crate::config;
 
-pub fn run(bin: &str, args: Args) {
+pub fn run(bin: &str, args: Args) -> Result<(), Report> {
     // no -c argument available in this case
-    let dir = config::install_to_use();
+    let dir = config::install_to_use(bin)?;
     let cmd = Path::new(&dir).join("bin").join(bin);
 
-    debug!("running {}", cmd.to_str().unwrap());
+    debug!("running {:?}", cmd);
 
-    let _ = Command::new(cmd.to_str().unwrap()).args(args).exec();
+    let mut binding = Command::new(cmd);
+    let cmd = binding.args(args);
+
+    exec(cmd)
+}
+
+#[cfg(unix)]
+fn exec(cmd: &mut Command) -> Result<(), Report> {
+    use std::os::unix::prelude::*;
+    Err(cmd.exec().into())
+}
+
+// thanks rustup command.rs
+#[cfg(windows)]
+fn exec(cmd: &mut Command) -> Result<(), Report> {
+    use windows_sys::Win32::Foundation::{BOOL, FALSE, TRUE};
+    use windows_sys::Win32::System::Console::SetConsoleCtrlHandler;
+
+    unsafe extern "system" fn ctrlc_handler(_: u32) -> BOOL {
+        // Do nothing. Let the child process handle it.
+        TRUE
+    }
+    unsafe {
+        if SetConsoleCtrlHandler(Some(ctrlc_handler), TRUE) == FALSE {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "Unable to set console handler",
+            ));
+        }
+    }
+
+    cmd.status()
 }
